97482f0b9d710140fae53db11be96a1a9ae2539e
vinigopackagemachine <>
2024-08-08 14:19:15 +0800

    ...

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1171945341"
+    serviceWorkerVersion: "3881878401"
   }
 });
